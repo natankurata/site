9c694fedf8d7994619f16fd09b0174fb6bd31ccd
Ademir Terin Junior <juniorterin0@gmail.com>
2021-05-12 16:10:42 -0300

    Mais uma arrumadinha e pronto

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,17 +9,6 @@
  */
 angular.module('siteNatanNovoApp')
   .controller('MainCtrl', function ($scope, $location, $anchorScroll) {
-    var elements1 = $('ul.tithome-compre-o-disco li').length;
-
-    for (var i = 0; i < elements1; i++) {
-      $(".tithome-compre-o-disco").clone().prependTo(".scorri1");
-    };
-
-    var elements2 = $('ul.tithome-spotify li').length;
-
-    for (var i = 0; i < elements2; i++) {
-      $(".tithome-spotify").clone().prependTo(".scorri2");
-    };
 
     $scope.gotoBottom = function (hash) {
       // set the location.hash to the id of
